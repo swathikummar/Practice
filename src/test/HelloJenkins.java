d24343c7ce137600f7adedec69bc54267afd96d9
swathikummar <swathi.k10@gmail.com>
2018-12-15 16:18:02 +0530

    first commit

---
+++ src/test/HelloJenkins.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author welcome
+ *
+ */
+public class HelloJenkins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins -- Swathi");
+	}
+
+}
