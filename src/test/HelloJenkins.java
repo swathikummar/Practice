a609e8c4449e898bf88810f4bacc55ac99b29ea5
swathikummar <swathi.k10@gmail.com>
2018-12-16 18:47:25 +0530

    added logger as "test git"

--- src/test/HelloJenkins.java
+++ src/test/HelloJenkins.java
@@ -15,6 +15,7 @@ public class HelloJenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Jenkins -- Swathi");
+		System.out.println("TestGIT");
 	}
 
 }
